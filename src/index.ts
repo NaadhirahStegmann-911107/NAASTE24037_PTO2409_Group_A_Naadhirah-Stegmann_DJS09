64145908984e9f0d99432c3d5a234d2a9c40a817
Naadhirah Stegmann <naadhirahsteg@gmail.com>
2025-06-21 22:35:23 +0200

    Loyalty confirmation
    Added the parameter for the use of a Boolean to confirm whether the reviewer in the array is Loyalty user or not.
    
    Also, removed the initial text as it does not serve a use any longer.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-// Number Types mini-challenge 10 10.2
-// Write a function that will only accept numbers and attend to
-// all TypeScript weakness flags.
-// : number
 const reviewTotalDisplay = document.querySelector("#reviews");
 
 const reviews = [
@@ -25,8 +21,9 @@ const reviews = [
   },
 ];
 
-function showReviewTotal (value : number, reviewer : string) {
-  reviewTotalDisplay.innerHTML = "review total " + value.toString() + " last reviewed by " + reviewer
+function showReviewTotal (value : number, reviewer : string, isLoyalty : boolean) {
+  const iconDisplay = isLoyalty ? "⭐" : " "
+  reviewTotalDisplay.innerHTML = "review total " + value.toString() + " last reviewed by " + reviewer + " " + iconDisplay
 }
 
-showReviewTotal(reviews.length, reviews[0].name)
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
